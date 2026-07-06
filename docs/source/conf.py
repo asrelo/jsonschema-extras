6f15fa93a846397c6fa973952aab1ed2b43c32cf
syrslava <syrslava@yandex.ru>
2026-07-06 04:20:23 +0300

    prepare for docs publishing to GH Pages

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,6 +44,7 @@ extensions = [
     'sphinx.ext.autodoc',
     #'sphinx.ext.autosummary',
     #'sphinx.ext.doctest',
+    'sphinx.ext.githubpages',
     'sphinx.ext.intersphinx',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
